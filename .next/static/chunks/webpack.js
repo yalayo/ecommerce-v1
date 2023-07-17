354e9a46ff38e65cab33999e045c921add219011
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-07-18 02:46:20 +1200

    fix audit to upgrade sanity, Cart component done

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "44ff8abd518051f7"; }
+/******/ 		__webpack_require__.h = function() { return "662d773236251711"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -87,7 +87,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c199a45d6dab8747"; }
+/******/ 	__webpack_require__.h = function() { return "782dbcb371b5660a"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
