cb74bd17cab98b27a673bee739721000370df377
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-07-20 10:16:43 +1200

    success landing page partially implemented

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a0bbdcd19745b77d"; }
+/******/ 		__webpack_require__.h = function() { return "2d5bd32faeb2603d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
