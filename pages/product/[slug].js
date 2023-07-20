faf61c6de35810cb2fe6b490bab84969df6f7981
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-07-21 11:24:46 +1200

    buy now button fully implemented

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -27,12 +27,17 @@ export const getStaticPaths = async () => {
     }
 }
 
+
 const ProductDetails = ({ product, products }) => {
     
     const { image, name, details, price } = product;
     const [index, setIndex] = useState(0);
+    const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
 
-    const { decQty, incQty, qty, onAdd } = useStateContext();
+    const handleBuyNow = () => {
+        onAdd(product, qty);
+        setShowCart(true);
+    }
 
     return (
         <div>
@@ -75,13 +80,13 @@ const ProductDetails = ({ product, products }) => {
                         <h3>Quantity: </h3>
                         <p className="quantity-desc">
                             <span className="minus" onClick={decQty}><AiOutlineMinus/></span>
-                            <span className="num" onClick="">{qty}</span>
+                            <span className="num" >{qty}</span>
                             <span className="plus" onClick={incQty}><AiOutlinePlus/></span>
                         </p>
                     </div>
                     <div className="buttons">
                         <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to cart</button>
-                        <button type="button" className="buy-now" onClick="">Buy Now</button>
+                        <button type="button" className="buy-now" onClick={handleBuyNow}>Buy Now</button>
                     </div> 
                 </div>
             </div>
